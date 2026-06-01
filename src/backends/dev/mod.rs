c86f93b31321028f1fde0a787a967a1d33c00659
XuChenXu <91937041+ChenXu233@users.noreply.github.com>
2026-06-01 18:21:26 +0800

    fix: update DevShell to use SessionREPL<Evaluator>

--- src/backends/dev/mod.rs
+++ src/backends/dev/mod.rs
@@ -8,9 +8,7 @@
 pub mod debugger;
 pub mod repl;
 pub mod shell;
-pub mod tui_repl;
 
 pub use debugger::{Debugger, DebuggerState};
-pub use repl::{REPL, REPLConfig, LineREPL, Evaluator, REPLBackend};
+pub use repl::{SessionREPL, Evaluator, REPLBackend};
 pub use shell::DevShell;
-pub use tui_repl::TuiREPL;
